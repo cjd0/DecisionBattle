d7cfd2e38e3bc2cbed5cef49d50ce320e2897ca6
Chris <chrisdoyle20@gmail.com>
2022-12-04 00:58:00 +0000

    Wrote code to define and assign UI variables, and link the activities

--- app/src/main/java/com/decisionbattle/Battle.java
+++ app/src/main/java/com/decisionbattle/Battle.java
@@ -2,13 +2,36 @@ package com.decisionbattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Battle extends AppCompatActivity {
+
+    //UI variables
+    Button btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+
+        //variable assignment
+        btnHelp = (Button)findViewById(R.id.btnBattleToHelp);
+
+        //set UI actions
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoToHelp();
+            }
+        });
+    }
+
+    //UI methods
+    public void GoToHelp(){
+        Intent intent = new Intent(this, Help.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/decisionbattle/MainActivity.java
+++ app/src/main/java/com/decisionbattle/MainActivity.java
@@ -2,13 +2,76 @@ package com.decisionbattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //UI variables
+    Button btnBattle, btnHoH, btnHelp, btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable assignment
+        btnBattle = (Button)findViewById(R.id.btnMenuToBattle);
+        btnHoH = (Button)findViewById(R.id.btnMenuToHoH);
+        btnHelp = (Button)findViewById(R.id.btnMenuToHelp);
+        btnSettings = (Button)findViewById(R.id.btnMenuToSettings);
+
+        //set UI actions
+        btnBattle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoToBattle();
+            }
+        });
+
+        btnHoH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoToHoH();
+            }
+        });
+
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoToHelp();
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoToSettings();
+            }
+        });
     }
+
+    //UI methods
+    public void GoToBattle(){
+        Intent intent = new Intent(this, Battle.class);
+        startActivity(intent);
+    }
+
+    public void GoToHoH(){
+        Intent intent = new Intent(this, HallOfHeroes.class);
+        startActivity(intent);
+    }
+
+    public void GoToHelp() {
+        Intent intent = new Intent(this, Help.class);
+        startActivity(intent);
+    }
+
+    public void GoToSettings(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
 }
